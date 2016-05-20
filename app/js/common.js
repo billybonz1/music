1be14d62011ff95352321e233eb14d0987714dac
Artem <artem21kalinichenko@gmail.com>
2016-05-20 23:10:21 +0300

    end

--- app/js/common.js
+++ app/js/common.js
@@ -46,7 +46,22 @@ $(document).ready(function() {
 	var owl = $(".carousel");
 	owl.owlCarousel({
 		items : 5,
-        autoHeight : true
+        autoHeight : true,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+
+			},
+			600:{
+				items:1,
+
+			},
+			1000:{
+				items:1,
+
+			}
+		}
 	});
 	owl.on("mousewheel", ".owl-wrapper", function (e) {
 		if (e.deltaY > 0) {
@@ -63,7 +78,22 @@ $(document).ready(function() {
 		owl.trigger("owl.prev");
 	});
     $(".main-slider").owlCarousel({
-        items : 1
+        items : 1,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+
+			},
+			600:{
+				items:1,
+
+			},
+			1000:{
+				items:1,
+
+			}
+		}
     });
 	//Кнопка "Наверх"
 	//Документация:
